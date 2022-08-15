fbed2a364be2d2058ccfe465668c56592801f006
Rachel Okorie <jayrachel96@gmail.com>
2022-08-15 08:28:38 +0100

    Update

--- TaskTwo/main.js
+++ TaskTwo/main.js
@@ -1,20 +1,41 @@
-const signIn = document.getElementById("signin");
-signIn.addEventListener('click', adminLogIn)
+let menuStore = [];
 
-function adminLogIn () {
-let username = prompt ("username")
-let password = prompt ("password")
-if ( username == "admin" && password == "admini"){
-alert ("Login successful"); 
-} 
-    else {
+function application () {
+    adminLogIn();
+
+}
+
+function adminLogIn() {
+  let username = prompt("username");
+  let password = prompt("password");
+  if (username == "admin" && password == "admini") {
+    alert("Login successful");
+    return true;
+  } else {
     alert("Invalid detail(s)");
-}
+    return false;
+  }
 }
 
-if (adminLogIn) {
-    editMenu
+function addFoodItem(name, price, description) {
+  let foodItem = {
+    id: menuStore.length,
+    name,
+    price,
+    description,
+  };
+
+  menuStore.push(foodItem);
 }
-function editMenu () {
-   
+
+function removeFoodItem(itemId) {
+  menuStore.splice(itemId, 1);
+}
+
+function getFoodItems() {
+  return menuStore;
+}
+
+function getFoodItem(itemId) {
+  return menuStore.filter((x) => x.id == itemId);
 }
