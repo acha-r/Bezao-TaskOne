14bc4b5cce9954a7abd7569d0663a729da59fc60
Rachel Okorie <jayrachel96@gmail.com>
2022-08-15 09:21:49 +0100

    Updated Task 2

--- TaskTwo/main.js
+++ TaskTwo/main.js
@@ -1,41 +1,78 @@
 let menuStore = [];
 
-function application () {
-    adminLogIn();
+function application() {
+    let authenticated = false;
+
+    while (!authenticated) {
+        authenticated = adminLogIn();
+    }
+
+    console.log()
+
+    let option = prompt(`
+    WELCOME
+    \n
+    ** 1: ADD FOOD ITEM\n
+    ** 2: VIEW FOOD ITEMS\n
+    ** 3: REMOVE FOOD ITEMS\n
+    `);
+
+    if (option == 1) {
+        let foodName = prompt("Food name");
+        let price = prompt("Food price");
+        let description = prompt("username");
+       
+        addFoodItem(foodName, price, description);
+        console.log("Added item successfully");
+        console.log(getFoodItems());
+    }
+    else if (option == 2) {
+        console.log(getFoodItems())
+    } else if (option == 3) {
+        let itemId = prompt("Food item Id");
+        
+        removeFoodItem(itemId)
+    }
+    else {
+        alert("Invalid Input")
+    }
+
 
 }
 
 function adminLogIn() {
-  let username = prompt("username");
-  let password = prompt("password");
-  if (username == "admin" && password == "admini") {
-    alert("Login successful");
-    return true;
-  } else {
-    alert("Invalid detail(s)");
-    return false;
-  }
+    let username = prompt("username");
+    let password = prompt("password");
+    if (username == "admin" && password == "admini") {
+        alert("Login successful");
+        return true;
+    } else {
+        alert("Invalid detail(s)");
+        return false;
+    }
 }
 
 function addFoodItem(name, price, description) {
-  let foodItem = {
-    id: menuStore.length,
-    name,
-    price,
-    description,
-  };
+    let foodItem = {
+        id: menuStore.length,
+        name,
+        price,
+        description,
+    };
 
-  menuStore.push(foodItem);
+    menuStore.push(foodItem);
 }
 
 function removeFoodItem(itemId) {
-  menuStore.splice(itemId, 1);
+    menuStore.splice(itemId, 1);
 }
 
 function getFoodItems() {
-  return menuStore;
+    return menuStore;
 }
 
 function getFoodItem(itemId) {
-  return menuStore.filter((x) => x.id == itemId);
+    return menuStore.filter((x) => x.id == itemId);
 }
+
+application() 
